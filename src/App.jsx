c87f3157c72e7c66c0a7ef09f3023f7f39593b3a
Harry MacCode <emarh.harry.code@gmail.com>
2024-03-04 00:42:56 +0100

    Logo and search part added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import './App.css'
+import './App.css';
+import Logo from './assets/images/logo.png';
+import { IoMdSearch } from "react-icons/io";
+
 
 const App = () => {
 
   return (
-    <div className='bg-red-500'>Test</div>
+    <div>
+      <header className='p-4 justify-between flex'>
+        <a href="" className='logo'>
+          <img src={Logo} alt="logo" className='w-66 h-10' />
+        </a>
+        <div className='flex gap-2 border border-gray-300 rounded-full py-2 px-4'>
+          <div>Anywhere</div>
+          <div className="border-l border-gray-300"></div>
+          <div>Any week</div>
+          <div className="border-l border-gray-300"></div>
+          <div>Add guest</div>
+          <button>
+            <IoMdSearch />
+          </button>
+        </div>
+      </header>
+    </div>
   )
 }
 
