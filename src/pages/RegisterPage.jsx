fcfd648075172b9b6755c07168a63f623d7b1963
Harry MacCode <emarh.harry.code@gmail.com>
2024-03-06 00:00:01 +0100

    Connexion to Backend

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,16 +1,25 @@
-// import React from 'react'
-
+import { useState } from "react"
 import { Link } from "react-router-dom"
+import axios from 'axios';
 
 const RegisterPage = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const registerUser = (e) => {
+    e.preventDefault();
+    axios.get('http://localhost:4080/test');
+  }
+
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
         <h1 className="text-4xl text-center">Register</h1>
-        <form action="" className="max-w-md mx-auto">
-            <input type="text" placeholder="John Doe" />
-            <input type="email" placeholder="you@email.com" />
-            <input type="password" placeholder="password" />
+        <form action="" className="max-w-md mx-auto" onSubmit={registerUser}>
+            <input type="text" placeholder="John Doe" value={name} onChange={e => setName(e.target.value)} />
+            <input type="email" placeholder="you@email.com" value={email} onChange={e => setEmail(e.target.value)} />
+            <input type="password" placeholder="password" value={password} onChange={e => setPassword(e.target.value)} />
             <button className="primary">Register</button>
             <div className="text-center py-2 text-gray-500">
                 Already an account ?
