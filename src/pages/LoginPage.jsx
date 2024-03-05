46a52d2a16bfcde5777a50be03e95e35d6b57bbe
Harry MacCode <emarh.harry.code@gmail.com>
2024-03-05 21:52:48 +0100

    Login Page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,22 @@
 // import React from 'react'
 
+import { Link } from "react-router-dom"
+
 const LoginPage = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="mt-4 grow flex items-center justify-around">
+      <div className="mb-64">
+        <h1 className="text-4xl text-center">Login</h1>
+        <form action="" className="max-w-md mx-auto">
+            <input type="email" placeholder="you@email.com" />
+            <input type="password" placeholder="password" />
+            <button className="primary">Login</button>
+            <div className="text-center py-2 text-gray-500">
+                Don't have an account yet?
+                <Link to='/register' className="underline text-black">Register Now</Link>
+            </div>
+        </form>
+      </div>
     </div>
   )
 }
